1b23cb344c343ac8d52fb80dab7dadad0fd72bc6
brahianpdev <zk.spywn@gmail.com>
2022-08-03 23:33:07 -0300

    feat(util): add set fields config

---
+++ src/utils/set.fields.ts
@@ -0,0 +1,12 @@
+// Internal modules
+import { commandConfig } from '../interfaces/command.config.interface';
+
+export function setFieldsConfig(commandsConfig: commandConfig) {
+	return commandsConfig.facts.map((value) => {
+		return {
+			name: value.command,
+			value: value.description,
+			inline: false,
+		};
+	});
+}
